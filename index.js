f4e2cd3adc3d2e80c3642d59330ee66215920ced
Tom Virgo <tom.virgo@Apollotyres.com>
2021-10-20 09:08:54 +0100

    add deployed client origin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 
 const io = new Server({
     cors: {
-      origin: ["http://localhost:4000"],
+      origin: ["http://localhost:4000", "https://virgo-chat.netlify.app"],
       methods: ["GET", "POST"]
     }});
 
